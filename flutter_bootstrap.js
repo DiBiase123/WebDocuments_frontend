0090da52775218d8114752d093924f9fa3c0bce6
Giuliano Di Biase <giulianofgfr84@gmail.com>
2026-07-10 16:44:52 +0200

    deploy: appbar respiro

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2488385840" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "802762321" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
